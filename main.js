808b0c301607ea5ff4229967acdbdef83e050069
Connor Folsom <cffolsom@gmail.com>
2022-07-01 13:47:39 -0700

    second commit

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ function popUp() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show"); }
 
-// Should be able to pull the images from the API since documentations mention them
+// By manually searching the above console logs, we are able to discern the image string
 // In this case, 2d484387-2509-5e8e-2c43-22f9981972eb is the artwork of the title
 
-// Plan to have data['data']['title'] in the popup
+// Plan to have data['data']['title'] in the popup.
 
 function popUpText() {
     var popText = document.getElementById("myPopup");
     popText
 }
+
